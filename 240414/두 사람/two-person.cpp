47ca6135a8134c7c13c46aa342a3506672b3a781
Mogu <166314448+funrace2@users.noreply.github.com>
2024-04-14 22:35:04 +0900

    Title: 두 사람, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240414/두 사람/two-person.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, c;
+    char b, d;
+    cin >> a >> b;
+    cin >> c >> d;
+    cout << ((a>=19 && b == 'M') || (c>=19 && b == 'M'));
+    return 0;
+}
