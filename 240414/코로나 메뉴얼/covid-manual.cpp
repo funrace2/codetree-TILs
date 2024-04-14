4cf50daf31e66601121b8c2fdd097ed965875378
Mogu <166314448+funrace2@users.noreply.github.com>
2024-04-14 23:43:03 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240414/코로나 메뉴얼/covid-manual.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a, c, e;
+    int b, d, f;
+    cin >> a >> b;
+    cin >> c >> d;
+    cin >> e >> f;
+    if ((a == 'Y' && b>=37) && (c == 'Y' && d>=37) && (e == 'Y' && f>=37)) {
+        cout << "E";
+    }
+    else if (a == 'Y' && b>=37) {
+        if ((e == 'Y' && f>=37) || (c == 'Y' && d>=37)) {
+            cout << "E";
+        }
+    }
+    else if (e == 'Y' && f>=37) {
+        if ((a == 'Y' && b>=37) || (c == 'Y' && d>=37)) {
+            cout << "E";
+        }
+    }
+    else if (c == 'Y' && d>=37) {
+        if ((a == 'Y' && b>=37) || (e == 'Y' && f>=37)) {
+            cout << "E";
+        }
+    }
+    else {
+        cout << "N";
+    }
+    return 0;
+}
